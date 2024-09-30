f39666b86a4e97967416acfa897d4395a9311732
regularyyy <126960109+regularyyy@users.noreply.github.com>
2024-09-30 22:58:09 +0300

    Update settings.py

--- news_website/settings.py
+++ news_website/settings.py
@@ -116,14 +116,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-    'C:/Users/Каролина/OneDrive/Pulpit/news/news_website/static',
-]
